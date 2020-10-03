3b9959103d6d427a12b3b3cb47957d1f1e924607
Sachin Kumar Gupta <sachingupta959@gmail.com>
2020-10-04 01:12:15 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const path = require('path');
+
+const express = require('express');
+const rootDir = require('./util/path');
+
+
+const app = express();
+
+// app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")))
+
+
+app.set('views', 'views');   
+
+app.get('/',(req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'index.html'));
+});
+
+
+
+app.listen(3000);
