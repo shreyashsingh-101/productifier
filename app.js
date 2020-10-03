7f447c436bcb20142a6cce2caa28e107d01f06aa
Shreyash Singh <Rohitsingh9450@hotmail.com>
2020-10-04 01:36:41 +0530

    Homepage

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const rootDir = require('./util/path');
 const app = express();
 
 // app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
-
+app.use(express.static(path.join(__dirname, "views")));
 app.set('views', 'views');   
 
 app.get('/',(req, res, next) => {
